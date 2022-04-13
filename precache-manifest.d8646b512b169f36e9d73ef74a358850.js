3bcdbda9b3c82eaf15c0c6974f278a45e051df20
Pintu kewat <pintookewat88@gmail.com>
2022-04-13 18:36:17 +0530

    Updates

---
+++ precache-manifest.d8646b512b169f36e9d73ef74a358850.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee1ce6fa095cfd9e99a5",
+    "url": "/pintoo/static/css/main.a4b54a64.chunk.css"
+  },
+  {
+    "revision": "ee1ce6fa095cfd9e99a5",
+    "url": "/pintoo/static/js/main.ee1ce6fa.chunk.js"
+  },
+  {
+    "revision": "fde4d7a599daaeb3391c",
+    "url": "/pintoo/static/css/1.1500ce83.chunk.css"
+  },
+  {
+    "revision": "fde4d7a599daaeb3391c",
+    "url": "/pintoo/static/js/1.fde4d7a5.chunk.js"
+  },
+  {
+    "revision": "cc23ae3a7343b7d68230",
+    "url": "/pintoo/static/js/runtime~main.cc23ae3a.js"
+  },
+  {
+    "revision": "62772273a2f6e4721707971ebe66ce62",
+    "url": "/pintoo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c701d7f46b3f4fa11c980459c28ed3dd.js"
+  "/pintoo/precache-manifest.d8646b512b169f36e9d73ef74a358850.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/pintoo/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
