daad1f325e756eaca3e7a6d6a48baad6058b2f82
Pintu kewat <91553565+pintookewat@users.noreply.github.com>
2022-04-14 21:37:23 +0530

    Update Menu.jsx

--- src/Componets/Menu.jsx
+++ src/Componets/Menu.jsx
@@ -16,25 +16,39 @@ import { useSelector } from "react-redux";
 export const Card = (props) => {
 
     const dis = useDispatch();
+    const Remove = () => {
+        const Element = document.getElementById("cards")
+        Element.remove();
+
+    };
+
+
+
+
 
     return (
 
         <>
 
-            <div className="cards">
+            <div className="cards" >
                 <div className=" shadow p-3 mb-5 bg-white rounded card" id="cards" >
+                    <Button variant="contained" color="error" onClick={Remove}> <RemoveIcon />Remove</Button><br />
+
                     <img src={props.img} class="card-img-top" alt="Food's" />
                     <div className="card-body">
                         <h5 className="card-title d-flex justify-content-center " style={{ "color": "grey" }}>{props.title}</h5>
                         <p class="card-text"> Cost : {props.cost}</p>
                         <p class="card-text">Number of items : {props.items} </p>
+
                         <div className="d-flex justify-content-between">
+
                             <Button variant="contained" color="error" onClick={() => dis(decNumber())}> <RemoveIcon /></Button>
                             <Button variant="contained" onClick={() => dis(incNumber())}><AddIcon /></Button>
                         </div>
                     </div>
                 </div>
             </div>
+
 
         </>
     )
@@ -43,8 +57,21 @@ export const Card = (props) => {
 
 const Menu = () => {
 
+    const Allremove = () => {
+        const RemoveAll = document.getElementById("Removeall")
+        RemoveAll.style.display ="none"
+
+    }
     return (
+
         <>
+
+            <div id="Removeall">
+            
+            <div className="d-flex justify-content-center">
+                <Button variant="contained" color="error" onClick={Allremove} >All Remove</Button>
+            </div>
+
                 {food.map((val) => {
 
                     let mystate = useSelector((state) => state.changeNumber);
@@ -53,12 +80,9 @@ const Menu = () => {
                         <Card img={val.img}
                             title={val.title}
                             cost={val.cost}
-                            items={mystate}/>)
+                            items={mystate} />)
 
                 })}
-
-            <div className="container-fluid  d-flex justify-content-center ">
-                <a href="/"> <Button className="shadow p-3 mb-5 rounded" variant="contained">Add To Cart</Button></a>
             </div>
 
 
